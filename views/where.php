0b7fe868e4d62b4b4527f45c1b14ead74f350340
jasonclark <jasclark@gmail.com>
2015-05-27 18:45:45 -0600

    set callback=? to force JSONP response, updated to jQuery 2.1.4

--- views/where.php
+++ views/where.php
@@ -1,7 +1,9 @@
 	<h2 class="result">Local topics for BookMeUp</h2>
 	<p id="message" style="display:none"><img src="./meta/img/loading.gif" id="loading" /> Waiting for Godot...</p>
 	<p class="terms"> </p>
-	<script async src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<!--<script async src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>-->
+  <script async src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
 <script type="text/javascript">
 	if (navigator.geolocation) {
 		//show loading message
@@ -18,23 +20,23 @@
 			//set latitude and longitude values
 			var lat = parseFloat(position.coords.latitude);
 			var lon = parseFloat(position.coords.longitude);
-			url = "http://experimental.worldcat.org/mapfast/services?geo=" + lat + "," + lon + ";crs=wgs84&radius=100000&mq=&sortby=distance&max-results=25";
+			url = "http://experimental.worldcat.org/mapfast/services?geo=" + lat + "," + lon + ";crs=wgs84&radius=100000&mq=&sortby=distance&max-results=25&callback=?";
 			$.getJSON(url, showSubjects);
 	}
-		
+
 	function showSubjects(data) {
 			$.each(data.Placemark, showSubject);
 			//hide loading message
 			var msg = document.getElementById("message");
 			msg.style.display = 'none';
 	}
-	
+
 	function showSubject(index, subject) {
 			//display results as html
-			var term = subject.name.replace(/ -- /g, " ");    
+			var term = subject.name.replace(/ -- /g, " ");
 			//var url = "http://www.worldcat.org/search?q=su:" + term + "&qt=advanced";
 			var url = "./index?view=search&q=" + term;
-			
+
 			html = '<a href="' + url + '">' + term + '</a> ';
 			$('.terms').append('<span>' + html + '</span>');
 	}
