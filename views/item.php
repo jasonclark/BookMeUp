8e871d42f7e665d19ba373732ce2799b68276101
jasonclark <jasclark@gmail.com>
2015-06-08 13:18:22 -0600

    added logic check for lccn & oclc number values, set up isbn for thumbnail query

--- views/item.php
+++ views/item.php
@@ -6,6 +6,9 @@ $worldcatKey = 'YOUR-WORLDCAT-API-KEY-HERE';
 
 $base = 'http://www.worldcat.org/webservices/catalog/content/isbn/'.$id.'?recordSchema=info%3Asrw%2Fschema%2F1%2Fdc&wskey='.$worldcatKey;
 
+// print out Worldcat API response for troubleshooting
+//echo $base;
+
 //build request and send to Worldcat Search API
 $request = simplexml_load_file($base);
 
@@ -15,19 +18,29 @@ $dc = $request->children('http://purl.org/dc/elements/1.1/');
 $title = $dc->title;
 
 if (strlen($title) > 0 ): //item available
-	
+
 	$title = $dc->title;
 	$author = $dc->creator;
 	$date = $dc->date;
 	$publisher = $dc->publisher;
 	$format = $dc->format;
-	$lccnNumber = trim($dc->identifier);
+	$isbn = trim($dc->identifier[0]);
 
 	$oclc = $request->children('http://purl.org/oclc/terms/');
-	$oclcNumber = trim($oclc->recordIdentifier);
 
-	$remoteImageUrl = 'http://covers.openlibrary.org/b/isbn/'.$id.'-S.jpg';
-	list($width, $height) = getimagesize($remoteImageUrl); 
+	//simple logic check for oclc and lccn values
+	if (strlen($oclc->recordIdentifier[1]) > 2) {
+		$oclcNumber = '/oclc/'.trim($oclc->recordIdentifier[1]);
+	} elseif (empty($oclc->recordIdentifier[1])) {
+		$oclcNumber = '/isbn/'.trim($dc->identifier[0]);
+	} else {
+		$oclcNumber = '/search?q=bn%3A'.$id.'&qt=advanced';
+	}
+
+	$lccnNumber = (strlen($oclc->recordIdentifier[0]) > 2) ? trim($oclc->recordIdentifier[0]) : $id;
+
+	$remoteImageUrl = 'http://covers.openlibrary.org/b/isbn/'.$isbn.'-S.jpg';
+	list($width, $height) = getimagesize($remoteImageUrl);
 	//echo $width;
 	if ($width > 30){
 		//thumbnail available
@@ -35,7 +48,7 @@ if (strlen($title) > 0 ): //item available
 	}else{
 		//set default thumbnail
 		$thumbnail = './meta/img/thumbnail-default.gif';
-	}	
+	}
 ?>
 
 <h2>Worldcat Item</h2>
@@ -47,7 +60,7 @@ by <?php echo $author; ?> (<?php echo $date; ?>)<br />
 <?php echo $format; ?><br />
 <?php echo $publisher; ?><br />
 <!--<a class="expand" href="http://www.worldcat.org/search?q=bn%3A<?php //echo $id; ?>&qt=advanced">Worldcat</a>-->
-<a class="expand" href="http://www.worldcat.org/oclc/<?php echo $oclcNumber; ?>">Worldcat</a>
+<a class="expand" href="http://www.worldcat.org<?php echo $oclcNumber; ?>">Worldcat</a>
 <a class="expand" href="http://lccn.loc.gov/<?php echo $lccnNumber; ?>">Library of Congress</a>
 </li>
 </ul>
