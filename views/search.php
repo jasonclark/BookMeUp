0dea397fb8186854b66b0d8f49ad828379195c50
jasonclark <jasclark@gmail.com>
2015-06-08 13:12:31 -0600

    set default thumbnail, standardized API & Search "loading" messages

--- views/search.php
+++ views/search.php
@@ -5,12 +5,12 @@ $key = isset($_GET['key']) ? trim(strip_tags(urlencode($_GET['key']))) : 'YOUR-W
 //set default value for query
 $q = isset($_GET['q']) ? trim(strip_tags(urlencode($_GET['q']))) : null;
 //set default value for latitude
-$lat = isset($_GET['lat']) ? $_GET['lat'] : null;
+//$lat = isset($_GET['lat']) ? $_GET['lat'] : null;
 //set default value for longitude
-$lng = isset($_GET['lng']) ? $_GET['lng'] : null;
+//$lng = isset($_GET['lng']) ? $_GET['lng'] : null;
 //set default value for library collection to search - list available at http://www.oclc.org/contacts/libraries/
 //docs here - http://oclc.org/developer/documentation/worldcat-search-api/library-catalog-url
-$library = isset($_GET['library']) ? trim(strip_tags($_GET['library'])) : 'YOUR-OCLC-LIBRARY-ID-HERE';
+$library = isset($_GET['library']) ? trim(strip_tags($_GET['library'])) : 'YOUR-OCLC-LIBRARY-ID-HERE;
 
 //include the Amazon Product services API class
 require_once './meta/inc/amazon-api-class.php';
@@ -43,11 +43,11 @@ else: //if form has query, show form and process
 <form id="searchBox" method="get" action="./index?view=search">
 <fieldset>
 <label for="q">Search</label>
-<input type="text" maxlength="200" name="q" id="q" tabindex="1" placeholder="keyword, isbn, title..." autofocus="autofocus" />
+<input type="text" maxlength="200" name="q" id="q" tabindex="1" placeholder="keyword, isbn, title..." autofocus />
 <button type="submit" id="btn" class="button">Search</button>
 </fieldset>
 </form>
-<p id="message" style="display:none"><img src="./meta/img/loading.gif" id="loading" /> loading...</p>
+<p id="message" style="display:none"><img src="./meta/img/loading.gif" id="loading" /> Time to make the donuts...</p>
 <script>
 window.onload = function() {
 var submit = document.getElementById('btn');
@@ -62,28 +62,28 @@ var submit = document.getElementById('btn');
 $Amazon=new Amazon();
 
 $parameters=array(
-"region"=>"com",
+"region" =>"com",
 "AssociateTag"=>"jasonclarkinf-20",
 "Operation"=>"ItemSearch", // we will be searching
 "SearchIndex"=>"Books", // "All" will search all categories, use "Books" to limit to books
-'ResponseGroup'=>'Images,ItemAttributes,EditorialReview,Reviews,Similarities',// we want images, item info, reviews, and related items
+//"RelationshipType"=>"AuthorityTitle", // specifies the means by which items are related to the one specified in the ItemLookup request
+//"ResponseGroup"=>"Images,ItemAttributes,EditorialReview,Reviews,RelatedItems,Similarities",// we want images, item info, reviews, and related items
+"ResponseGroup"=>"Images,ItemAttributes,EditorialReview,Reviews,Similarities",// we want images, item info, reviews, and related items
 "Keywords"=>"$q"); // this is what we are looking for, you could use the book's title instead
 
 $queryUrl=$Amazon->getSignedUrl($parameters);
 
-$request=simplexml_load_file($queryUrl) or die ("xml response not loading");
+$request=simplexml_load_file($queryUrl) or die ('API response not loading');
 
-if($request->Items->TotalResults > 0)
-{
+if($request->Items->TotalResults > 0) {
 
-echo $request;
 // we have at least one response
 //set Amazon xml values as specifc variables to be printed out below
 $image = $request->Items->Item->SmallImage->URL;
-if (empty($image)) { $image = '* Image Not Available'; }
+if (empty($image)) { $image = './meta/img/thumbnail-default.gif'; }
 $title = $request->Items->Item->ItemAttributes->Title;
 $author = $request->Items->Item->ItemAttributes->Author;
-//simple logic check for author and director values, shows
+//simple logic check for author and director values
 if (strlen($author) > 2) {
         $creator = $author;
 } elseif (empty($author)) {
@@ -97,7 +97,7 @@ $editorialReview = $request->Items->Item->EditorialReviews->EditorialReview->Con
 $CustomerReviews = $request->Items->Item->CustomerReviews->IFrameURL;
 
 //print out Amazon xml values as html
-echo '<h3>Whoah.. If you are reading...</h3>'."\n";
+echo '<h3>Hmmm... If you are reading...</h3>'."\n";
 echo '<ul class="item" >'."\n";
 echo '<li>'."\n";
 echo '<img src="'.$image.'" />'."\n";
