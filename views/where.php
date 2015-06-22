c6556295c7ab35cb406608ddd5ca998a5914c163
jasonclark <jasclark@gmail.com>
2015-06-21 22:59:45 -0600

    fixed callback error on MapFast API call; optimized jQuery .getJSON

--- views/where.php
+++ views/where.php
@@ -1,9 +1,7 @@
 	<h2 class="result">Local topics for BookMeUp</h2>
 	<p id="message" style="display:none"><img src="./meta/img/loading.gif" id="loading" /> Waiting for Godot...</p>
 	<p class="terms"> </p>
-  <script async src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-
-<script type="text/javascript">
+  <script type="text/javascript">
 	if (navigator.geolocation) {
 		//show loading message
 		var msg = document.getElementById("message");
@@ -16,29 +14,40 @@
 	}
 
 	function getSubjects(position) {
-			//set latitude and longitude values
-			var lat = parseFloat(position.coords.latitude);
-			var lon = parseFloat(position.coords.longitude);
-			url = "http://experimental.worldcat.org/mapfast/services?geo=" + lat + "," + lon + ";crs=wgs84&radius=100000&mq=&sortby=distance&max-results=25&callback=?";
-			$.getJSON(url, showSubjects);
+		//set latitude and longitude values
+		var lat = parseFloat(position.coords.latitude);
+		var lon = parseFloat(position.coords.longitude);
+		var url = "http://experimental.worldcat.org/mapfast/services?geo=" + lat + "," + lon + ";crs=wgs84&radius=100000&mq=&sortby=distance&max-results=30&callback=?";
+		$.getJSON(url, showSubjects);
+
+		/*$.ajax({
+      type: 'GET',
+      url: url,
+      contentType: 'application/json',
+      dataType: 'jsonp',
+      crossDomain: true,
+      xhrFields: { withCredentials: true }
+    }).done(showSubjects);*/
+
 	}
 
 	function showSubjects(data) {
-			$.each(data.Placemark, showSubject);
-			//hide loading message
-			var msg = document.getElementById("message");
-			msg.style.display = 'none';
+    $.each(data.Placemark, showSubject);
+		//hide loading message
+		var msg = document.getElementById("message");
+		msg.style.display = 'none';
 	}
 
 	function showSubject(index, subject) {
-			//display results as html
-			var term = subject.name.replace(/ -- /g, " ");
-			//var url = "http://www.worldcat.org/search?q=su:" + term + "&qt=advanced";
-			var url = "./index?view=search&q=" + term;
+		//display results as html
+		var term = subject.name.replace(/ -- /g, " ");
+		//var url = "http://www.worldcat.org/search?q=su:" + term + "&qt=advanced";
+		var url = "./index?view=search&q=" + term;
 
-			html = '<a href="' + url + '">' + term + '</a> ';
-			$('.terms').append('<span>' + html + '</span>');
+		html = '<a href="' + url + '">' + term + '</a> ';
+		$('.terms').append('<span>' + html + '</span>');
 	}
 	</script>
+  <script async src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<a class="bck" href="./index.php?view=search">home</a>
 
