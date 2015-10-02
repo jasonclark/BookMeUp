874795e86b483885f9f991a44dbb13b9c74a2406
Jason A. Clark <jasclark@gmail.com>
2015-10-01 23:23:32 -0600

    removed jQuery dependency, created JSONP routine for MapFAST API

--- views/where.php
+++ views/where.php
@@ -1,53 +1,42 @@
 	<h2 class="result">Local topics for BookMeUp</h2>
 	<p id="message" style="display:none"><img src="./meta/img/loading.gif" id="loading" /> Waiting for Godot...</p>
 	<p class="terms"> </p>
-  <script type="text/javascript">
-	if (navigator.geolocation) {
-		//show loading message
-		var msg = document.getElementById("message");
-		msg.style.display = 'block';
-		navigator.geolocation.getCurrentPosition(getSubjects);
-		console.log('geolocation done');
-	} else {
-		//onError();
-		alert('Error: Your browser doesn\'t support geolocation.');
-	}
+        <script id="worker" type="text/javascript">
+if (navigator.geolocation) {
+        //show loading message
+        var msg = document.getElementById("message");
+        msg.style.display = 'block';
+        navigator.geolocation.getCurrentPosition(setScript);
+        console.log('geolocation done');
+} else {
+        //onError();
+        alert('Error: Your browser doesn\'t support geolocation.');
+}
 
-	function getSubjects(position) {
-		//set latitude and longitude values
-		var lat = parseFloat(position.coords.latitude);
-		var lon = parseFloat(position.coords.longitude);
-		var url = "https://experimental.worldcat.org/mapfast/services?geo=" + lat + "," + lon + ";crs=wgs84&radius=100000&mq=&sortby=distance&max-results=30&callback=?";
-		$.getJSON(url, showSubjects);
+function setScript(position) {
+        //set latitude and longitude values
+        var lat = parseFloat(position.coords.latitude);
+        var lon = parseFloat(position.coords.longitude);
+        var script = document.createElement('script');
+        script.src = "https://experimental.worldcat.org/mapfast/services?geo=" + lat + "," + lon + ";crs=wgs84&radius=100000&mq=&sortby=distance&max-results=30&callback=showSubjects";
+        document.querySelector('.terms').appendChild(script);
+        //document.getElementsByTagName('body')[0].appendChild(script);
+}
 
-		/*$.ajax({
-      		type: 'GET',
-      		url: url,
-      		contentType: 'application/json',
-      		dataType: 'jsonp',
-      		crossDomain: true,
-      		xhrFields: { withCredentials: true }
-    		}).done(showSubjects);*/
-
-	}
-
-	function showSubjects(data) {
-    $.each(data.Placemark, showSubject);
-		//hide loading message
-		var msg = document.getElementById("message");
-		msg.style.display = 'none';
-	}
-
-	function showSubject(index, subject) {
-		//display results as html
-		var term = subject.name.replace(/ -- /g, " ");
-		//var url = "http://www.worldcat.org/search?q=su:" + term + "&qt=advanced";
-		var url = "./index?view=search&q=" + term;
-
-		html = '<a href="' + url + '">' + term + '</a> ';
-		$('.terms').append('<span>' + html + '</span>');
-	}
-	</script>
-  <script async src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+function showSubjects(data) {
+        //hide loading message
+        var msg = document.getElementById("message");
+        msg.style.display = 'none';
+        var container = document.getElementById("terms");
+        var markup = '<span>';
+        //var url = "http://www.worldcat.org/search?q=su:" + term + "&qt=advanced";
+        var url = "./index?view=search&q=";
+        for (var i = 0; i < data.Placemark.length; i++) {
+                var cleanTerm = data.Placemark[i].name.replace(/ -- /g, " ");
+                markup += '<a title="' + data.Placemark[i].name + '" href="' + url + cleanTerm + '">' + data.Placemark[i].name + '</a>';
+        }
+        container.innerHTML = markup + '</span>';
+}
+        </script>
 	<a class="bck" href="./index.php?view=search">home</a>
 
