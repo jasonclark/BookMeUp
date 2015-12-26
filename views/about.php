3de7baa85e09af31ed30ea3cba3eafc1733cd875
Jason A. Clark <jasclark@gmail.com>
2015-12-26 03:32:02 -0700

    added reference to home page and link to source code

--- views/about.php
+++ views/about.php
@@ -1,5 +1,5 @@
 <h2>About BookMeUp</h2>
 <ul>
 <li>"BookMeUp" uses the <a class="amazon" href="https://affiliate-program.amazon.com/gp/advertising/api/detail/main.html">Amazon Product Advertising API</a> to suggest related books to read based on a user's location and/or search query.</li>
-<li>This app is built with php, javascript, and html and optimized for mobile environments. Other APIs used for data sources include: MapFast from OCLC, the Worldcat Search API, and the Open Library Cover Image API.</li>
+<li>This app is built with php, javascript, and html and optimized for mobile environments. Other APIs used for data sources include: MapFast from OCLC, the Worldcat Search API, and the Open Library Cover Image API. Source code is available at <a href="http://www.jasonclark.info">jasonclark.info</a> and on <a href="https://github.com/jasonclark/BookMeUp">GitHub</a>.</li>
 </ul>
